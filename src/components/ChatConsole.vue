207a8b55b08502e6f9ffe3405e7582af420ef47e
Marian Zwarg <marian.zwarg@gmx.de>
2021-07-30 14:30:58 +0200

    added ChatConsole component

---
+++ src/components/ChatConsole.vue
@@ -0,0 +1,48 @@
+<template>
+  <transition name="fade">
+    <div class="hello" v-if="getStateData.isLoggedIn">
+      <h1>Hello {{ getStateData.usrname }}</h1>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "ChatConsole",
+  props: {
+    msg: String
+  },
+  data(){
+    return {
+      displaying: true,
+      usrname: String
+    }
+  },
+  computed: {
+    getStateData () {
+      return {
+          isLoggedIn: this.$store.state.loggedIn,
+          usrname: this.$store.state.usrname
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ChatConsole msg="Welcome to the chat"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import ChatConsole from "@/components/ChatConsole.vue";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
+    ChatConsole
   },
+  methods:{
+    speak(){
+      console.log("I said hi!");
+    }
+  }
 };
 </script>

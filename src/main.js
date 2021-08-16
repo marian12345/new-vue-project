df9ef35401a875053477411e7e018591890321d1
Marian Samuel Zwarg Student-COM <mz17aao@herts.ac.uk>
2021-08-16 13:59:34 +0200

    add tailwind css

--- src/main.js
+++ src/main.js
@@ -4,38 +4,8 @@ import router from "./router";
 import store from "./store";
 import VueSocketIOExt from "vue-socket.io-extended";
 import { io } from "socket.io-client";
+import "./index.css";
 
-/**
- * Also possible:
- * 
- * 
-//const options = {};
-createApp(App).use(new VueSocketIO({
-    debug: true,
-    connection: SocketIO('http://localhost:3000', options),
-    vuex: {
-      store,
-      actionPrefix: "SOCKET_",
-      mutationPrefix: "SOCKET_"
-    }
-  })).use(store).use(router).mount("#app");
-
-  or 
-
-  .use(
-    new VueSocketIO({
-      debug: true,
-      connection: "http://localhost:3000",
-      vuex: {
-        store,
-        actionPrefix: "SOCKET_",
-        mutationPrefix: "SOCKET_",
-      },
-      options: {}, //Optional options
-    })
-  )
-* 
- */
 const socket = io("http://localhost:3000");
 
 createApp(App)
